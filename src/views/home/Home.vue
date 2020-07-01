700470562c066e48222975a788d128d481cd6a2d
wgq1130254587 <1130254587@qq.com>
2020-07-01 15:53:38 +0800

    添加推荐信息

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="home">
-      <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+      <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+      <HomeSwiper :banners="banners"/>
+      <RecommendView :recommends="recommends"/>
   </div>
 </template>
 
 <script>
     import NavBar from "@/components/common/navbar/NavBar";
-  export default {
+    import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+    import {getHomeMultidata} from "@/notwork/home";
+    import RecommendView from "@/views/home/childComps/RecommendView";
+
+
+    export default {
     name: "Home",
     components:{
-      NavBar
+      NavBar,
+      HomeSwiper,
+      RecommendView
+
+    },
+      data(){
+      return {
+        banners:[],
+        recommends:[]
+      }
+      },
+      created() {
+
+        getHomeMultidata().then(res =>{
+          // console.log(res);
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      }
     }
-  }
 </script>
 
 <style scoped>
